a5447004dfb31edbe71749117d810a9580062a91
Андрей <gmgm.ab@gmail.com>
2020-08-31 13:44:59 +0700

    Updates

---
+++ precache-manifest.1b35b7305f79c2fd2992457c4ac2de40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbab62eafad4116656b46bee52b369a",
+    "url": "/react-table-app/index.html"
+  },
+  {
+    "revision": "ef6d1a0ba9802d64dd83",
+    "url": "/react-table-app/static/css/main.6c2894b6.chunk.css"
+  },
+  {
+    "revision": "69b7f7fc1b7f6b6b9584",
+    "url": "/react-table-app/static/js/2.09332a82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-table-app/static/js/2.09332a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef6d1a0ba9802d64dd83",
+    "url": "/react-table-app/static/js/main.f496b1e1.chunk.js"
+  },
+  {
+    "revision": "b8335b20fad857fc7515",
+    "url": "/react-table-app/static/js/runtime-main.58bf0e6f.js"
+  }
+]);
